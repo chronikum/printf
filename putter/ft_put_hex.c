0fd4878c92a02d19d6cd6749c6b32b2fde47966c
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-07-13 17:06:54 +0200

    norm

--- putter/ft_put_hex.c
+++ putter/ft_put_hex.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 10:44:40 by jfritz            #+#    #+#             */
-/*   Updated: 2021/07/13 17:02:19 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/07/13 17:06:44 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_put_hex(unsigned long long p, int u)
 {
 	if (p == 0)
-		return ft_putstr("0");
+		return (ft_putstr("0"));
 	if (u == 1)
 		return (ft_dec_to_hex(p, 0, 1));
 	return (ft_dec_to_hex(p, 1, 1));
